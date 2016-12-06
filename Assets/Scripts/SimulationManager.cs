aea2f8ad1424e1ccec2eaf9e81b6d8eaa7f42681
Camille78 <camille.soulie19@gmail.com>
2016-12-06 18:05:04 +0100

    Commit d'une scene, des codes TCPserver, AuvBehaviour, CameraBehaviour et SimulationManager

---
+++ Assets/Scripts/SimulationManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SimulationManager : MonoBehaviour
+{
+	public static SimulationManager manager;
+	public GameObject[] auvs;
+	public GameObject[] buoys;
+	//public int totalAuv;
+	//public int totalBuoy;
+
+	void Awake()
+	{
+		manager = this;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	public static GameObject[] getAuvs()
+	{
+		return manager.auvs.ToArray();
+	}
+
+	public static GameObject getBuoys(int index)
+	{
+		return manager.buoys[index];
+	}
+}
+
---
+++ Assets/Scripts/TCPserver.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Text;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+public class TCPserver:MonoBehaviour{
+	public bool isConnection;
+	public Thread mThread;
+	public TcpListener server;
+	private string data;
+	private Queue<String> TaskQueue = new Queue<String>();
+	private object _queueLock = new object();
+	//private Transform target;
+	private Vector3 target;
+	private GameObject[] auvs; 
+
+	// Use this for initialization
+	void Start(){
+		isConnection=false;
+		auvs= SimulationManager.getAuvs();
+
+		print ("StartThread");
+		ThreadStart ts = new ThreadStart(Update1);
+		mThread = new Thread(ts);
+		mThread.Start();
+
+	}
+	void Update () {
+		lock (_queueLock)
+		{
+			if (TaskQueue.Count > 0) {
+				string data=TaskQueue.Dequeue ();
+				updateAuv (data);
+			}
+		}
+	}
+
+	void OnApplicationQuit(){
+		//server.Stop();
+		mThread.Abort (); 
+	}
+
+	// Update is called once per frame
+	void Update1()
+	{  
+		TcpListener server=null;  
+		try
+		{
+			// Set the TcpListener on port 13000.
+			Int32 port = 13000;
+			IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+			// TcpListener server = new TcpListener(port);
+			server = new TcpListener(localAddr, port);
+
+			// Start listening for client requests.
+			server.Start();
+
+			// Buffer for reading data
+			Byte[] bytes = new Byte[256];
+			data = null;
+
+			// Enter the listening loop.
+			while(true)
+			{
+				Thread.Sleep(10);
+				Debug.Log("Waiting for a connection... ");
+
+				// Perform a blocking call to accept requests.
+				// You could also user server.AcceptSocket() here.
+				TcpClient client = server.AcceptTcpClient();  
+				if(client!=null){
+
+					Debug.Log("Connected!");
+				}
+
+				data = null;
+
+				// Get a stream object for reading and writing
+				NetworkStream stream = client.GetStream();
+
+				int i;
+
+				// Loop to receive all the data sent by the client.
+				while((i = stream.Read(bytes, 0, bytes.Length))!=0)
+				{  
+					// Translate data bytes to a ASCII string.
+					data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+					Debug.Log("Received:"+ data);
+
+					//add situation message to queue
+					lock (_queueLock)
+					{
+						if (TaskQueue.Count < 100)
+							TaskQueue.Enqueue(data);
+					}
+
+					// Process the data sent by the client.
+					data = data.ToUpper();
+
+					byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+
+					// Send back a response.
+					stream.Write(msg, 0, msg.Length);
+					Debug.Log("Sent:"+ data);          
+				}
+
+				// Shutdown and end connection
+				client.Close();
+			}
+		}
+		catch(SocketException e)
+		{
+			Debug.Log("SocketException:"+ e);
+		}
+		finally
+		{
+			// Stop listening for new clients.
+			server.Stop();
+		}
+
+		//yield return null;
+
+
+	}
+
+	void updateAuv(string data)
+	{
+		Debug.Log ("enter update");
+
+		//Auv indexAuv speed x y z
+		string[] splitData = data.Split (" "[0]);
+		int indexAuv = int.Parse(splitData [1]);
+		int speed = int.Parse(splitData [2]);
+		Vector3 target = new Vector3 (int.Parse(splitData [3]), int.Parse(splitData [4]), int.Parse(splitData [5]));
+
+		auvs[indexAuv].GetComponent<AuvBehaviour>().setSpeed(speed);
+		auvs[indexAuv].GetComponent<AuvBehaviour>().setTarget (target);
+
+		Debug.Log("Order : auv  "+ indexAuv+"  move to " + target+" speed  : "+speed);
+	}
+}	
