5decbe7cc3f31b98e2cc4501467e8dfee9737ba4
Camille78 <camille.soulie19@gmail.com>
2017-01-20 10:01:44 +0100

    Add files via upload

---
+++ client-api/src/config.h
@@ -0,0 +1,22 @@
+//
+// Created by tag on 16/12/16.
+//
+
+#pragma once
+
+// Interface
+#define LARGEUR_FENETRE 900
+#define HAUTEUR_FENETRE 700
+#define DELAY_FRAME 30
+
+// World
+#define DT 0.2 // s
+#define GRAV_CONST 9.81 // m/s²
+#define BOAT_NUMBER 4
+#define BUOY_NUMBER 5
+#define X_BOAT_TARGET {100,-100, 100,-100}
+#define Y_BOAT_TARGET {100, 100,-100,-100}
+
+// Buoy
+#define BUOY_MASS 1 // kg
+#define BUOY_VOLUME 1 // m³
